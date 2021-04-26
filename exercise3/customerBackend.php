1e830e9bcdf73e89fbd9d1c40835b891c8e0d454
GriffinKeeter <30298637+GriffinKeeter@users.noreply.github.com>
2021-04-25 21:41:32 -0500

    Update customerBackend.php

--- exercise3/customerBackend.php
+++ exercise3/customerBackend.php
@@ -1,13 +1,13 @@
 <?php
 	echo "Thank you for your purchase, this is your password: ";
-    $ps = "password";//$_POST["password"];
+    $ps = $_POST["password"];
     echo password;
     $total = 0;
 	for($x = 0; $x < 6; $x ++){
     	for($y = 0; $y < 6; $y ++){
         	$num = (6*$x + $y);
         	$name = "Basket" . $num;
-            if(1){//if($_POST[$name] == $name){
+            if($_POST[$name] == $name){
             	echo "<br>";
             	echo "item: " . ($total + 1) . ": " . $name . "   ";
                 echo " - - - cost: $1";
@@ -16,7 +16,7 @@
    		}
     }	
     
-    $ship = "free";//$_POST["shipping"];
+    $ship = $_POST["shipping"];
     echo "<br>Shipping: " . $ship;
     echo "<br>total cost: $" . $total . " not including shipping";
     
